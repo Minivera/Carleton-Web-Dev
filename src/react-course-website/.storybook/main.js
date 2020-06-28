8f8a64a4412896e34cf53641f70b2e1f963ec931
guillaume <guillaume@codecov.io>
2020-06-28 19:17:51 -0400

    babelrc was needed for react

--- src/react-course-website/.storybook/main.js
+++ src/react-course-website/.storybook/main.js
@@ -2,21 +2,7 @@ module.exports = {
   stories: ['../stories/**/*.stories.(js|jsx|ts|tsx|mdx)'],
   addons: [
     "@storybook/addon-actions",
-    {
-      name: "@storybook/addon-docs",
-      options: {
-        babelOptions: {
-          presets: [
-            [
-              "@vue/cli-plugin-babel/preset",
-              {
-                jsx: false
-              }
-            ]
-          ]
-        }
-      }
-    },
+    "@storybook/addon-docs",
     "@storybook/addon-knobs",
     "@storybook/addon-links",
     "@storybook/addon-notes"
